928a3ffd8aa1947f63af75c562a5d04e563ac55f
MAALA <maalashree.ece5@gmail.com>
2024-07-13 07:40:54 +0530

    Datatypes

---
+++ Class_10062024/Code_033.py
@@ -0,0 +1,21 @@
+# Program - Calculate the area of circle.
+# input -> radius
+# output -> area
+import math
+
+# data types
+# input -> int or float -> float
+# output -> float
+
+# Core Logic -> pi*r*r = 3.14
+
+radius = float(input("Enter the radius\n"))
+print(math.pi)
+area = math.pi*(radius**2)
+area2 = math.pi*(math.pow(radius,2))
+print(area)
+print(area2)
+
+# math.pow(radius,3)
+ex=math.pow(2,3)
+print(ex)
